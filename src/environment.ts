46da709e107b796352ebd25314f57bec42435e9e
Vishwas-guptaa <vishwasgupta81779@gmail.com>
2024-09-04 15:16:17 +0530

    fix

--- src/environment.ts
+++ src/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     // Your API URL for local
     //  apiUrl: 'http://127.0.0.1:8000/api/'
-     apiUrl: 'https://api.crrescita.com/api',
+     apiUrl: 'https://api.crrescita.com/api/',
     
   };
   
